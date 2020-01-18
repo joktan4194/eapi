026180a3e04a885f968a9558805daf8f3a2e391c
joktan4194 <joktanokpanachi@yahoo.com>
2020-01-18 04:17:06 +0100

    Product collection resource created

--- app/Http/Resources/Collection/Product/ProductCollection.php
+++ app/Http/Resources/Collection/Product/ProductCollection.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Resources\Collection;
+namespace App\Http\Resources\Collection\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,12 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return
+        ['name'=>$this->name,
+         'discount'=>$this->discount.'%',
+         'total_price'=>round(( (1- ($this->discount/100) ) * $this->price),2),
+         'href'=>
+             ['link'=>route('products.show',$this->id)]
+         ];
     }
 }
